c035942ebd7bf7907b8bde7e71946fe1c83beb1a
leon.martin <leon@home>
2020-05-20 22:40:35 +0200

    multi selection is now supported

--- src/extension.ts
+++ src/extension.ts
@@ -1,87 +1,5 @@
 import * as vscode from "vscode";
 
-function moveSelection(
-  direction: String,
-  editor: vscode.TextEditor,
-  selection: vscode.Selection
-) {
-  // get selected text
-  const selStart = selection.start;
-  const selEnd = selection.end;
-  const selText = editor.document.getText(selection);
-  if (selText === "") {
-    return;
-  }
-
-  console.log("alt+" + direction + ":\t" + selText);
-
-  //
-  //
-  // ALT+RIGHT
-  if (direction === "right") {
-    // get next char
-    const nextChar = editor.document.getText(
-      new vscode.Range(selEnd, selEnd.translate(0, 1))
-    );
-
-    // return if there is no next char
-    if (nextChar === "") {
-      return;
-    }
-
-    // edit text
-    editor.edit((textEditor) => {
-      // temporarly delete selected text
-      textEditor.delete(new vscode.Range(selStart, selEnd));
-
-      // insert selected text one character to the right
-      textEditor.insert(selEnd.translate(0, 1), selText);
-    });
-
-    // update selection in editor
-    editor.selection = new vscode.Selection(
-      selStart.translate(0, 1),
-      selStart.translate(0, selText.length + 1)
-    );
-
-    //
-    //
-    // ALT+LEFT
-  } else if (direction === "left") {
-    // return if the selection starts at the beginning of the line
-    if (editor.document.offsetAt(selStart) === 0) {
-      return;
-    }
-    
-    // return if translating -1 fails, i.e., there is no previous position
-    try {
-      selStart.translate(0, -1);
-    } catch (e) {
-      return;
-    }
-    
-    // get previous char
-    const prevChar = editor.document.getText(
-      new vscode.Range(selStart.translate(0, -1), selStart)
-    );
-
-    // edit text
-    editor.edit((textEditor) => {
-      // temporarly delete previous char
-      textEditor.delete(new vscode.Range(selStart.translate(0, -1), selStart));
-
-      // insert previous character after selected text
-      textEditor.insert(selEnd, prevChar);
-    });
-
-    // update selection in editor
-    editor.selection = new vscode.Selection(
-      selStart.translate(0, -1),
-      selEnd.translate(0, -1)
-    );
-  }
-}
-
 function moveSelections(direction: String) {
   // get active text editor
   const editor = vscode.window.activeTextEditor;
@@ -96,10 +14,86 @@ function moveSelections(direction: String) {
 
   const currentSelections = editor.selections;
 
-  // TODO: only the moving of the first selection is excuted; is possibly caused by promise of edit method
-  currentSelections.forEach((selection) => {
-    moveSelection(direction, editor, selection);
-  });
+  //
+  //
+  // ALT+RIGHT
+  if (direction === "right") {
+    // edit text
+    editor.edit((textEditor) => {
+      currentSelections.forEach((selection) => {
+        const selStart = selection.start;
+        const selEnd = selection.end;
+        const selText = editor.document.getText(selection);
+
+        // get next char
+        const nextChar = editor.document.getText(
+          new vscode.Range(selEnd, selEnd.translate(0, 1))
+        );
+        console.log(nextChar);
+
+        // return if there is no next char
+        if (nextChar === "") {
+          return;
+        }
+
+        // temporarly delete selected text
+        textEditor.delete(new vscode.Range(selStart, selEnd));
+
+        // insert selected text one character to the right
+        textEditor.insert(selEnd.translate(0, 1), selText);
+
+        // TODO: update selection in editor
+        editor.selection = new vscode.Selection(
+          selStart.translate(0, 1),
+          selStart.translate(0, selText.length + 1)
+        );
+      });
+    });
+
+    //
+    //
+    // ALT+LEFT
+  } else if (direction === "left") {
+    // edit text
+    editor.edit((textEditor) => {
+      currentSelections.forEach((selection) => {
+        const selStart = selection.start;
+        const selEnd = selection.end;
+        const selText = editor.document.getText(selection);
+
+        // return if the selection starts at the beginning of the line
+        if (editor.document.offsetAt(selStart) === 0) {
+          return;
+        }
+
+        // return if translating -1 fails, i.e., there is no previous position
+        try {
+          selStart.translate(0, -1);
+        } catch (e) {
+          return;
+        }
+
+        // get previous char
+        const prevChar = editor.document.getText(
+          new vscode.Range(selStart.translate(0, -1), selStart)
+        );
+
+        // temporarly delete previous char
+        textEditor.delete(
+          new vscode.Range(selStart.translate(0, -1), selStart)
+        );
+
+        // insert previous character after selected text
+        textEditor.insert(selEnd, prevChar);
+
+        // TODO: update selection in editor
+        editor.selection = new vscode.Selection(
+          selStart.translate(0, -1),
+          selEnd.translate(0, -1)
+        );
+      });
+    });
+  }
 }
 
 export function activate(context: vscode.ExtensionContext) {
